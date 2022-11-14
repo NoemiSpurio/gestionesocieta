b98879014455c48c7c21e0e8a3385a37cd88afd6
NoemiSpurio <noemi.spurio@gmail.com>
2022-11-14 14:46:24 +0100

    implementazione test inserimento societa

--- src/main/java/it/prova/gestionesocieta/GestionesocietaApplication.java
+++ src/main/java/it/prova/gestionesocieta/GestionesocietaApplication.java
@@ -1,20 +1,29 @@
 package it.prova.gestionesocieta;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import it.prova.gestionesocieta.service.BatteriaDiTestService;
+
 @SpringBootApplication
 public class GestionesocietaApplication implements CommandLineRunner {
 
+	@Autowired
+	private BatteriaDiTestService batteriaDiTestService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GestionesocietaApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
+		System.out.println("=== INIZIO BATTERIA DI TEST ===");
 		
+		batteriaDiTestService.testInserisciNuovaSocieta();
+		
+		System.out.println("=== FINE BATTERIA DI TEST ===");
 	}
 
 }
--- src/main/java/it/prova/gestionesocieta/service/BatteriaDiTestService.java
+++ src/main/java/it/prova/gestionesocieta/service/BatteriaDiTestService.java
@@ -1,8 +1,32 @@
 package it.prova.gestionesocieta.service;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import it.prova.gestionesocieta.model.Societa;
 
 @Service
 public class BatteriaDiTestService {
 
+	@Autowired
+	private SocietaService societaService;
+	
+	@Autowired
+	private DipendenteService dipendenteService;
+	
+	public void testInserisciNuovaSocieta() {
+		
+		Societa nuovaSocieta = new Societa("Ragione sociale 1","via Colombo 33", new Date());
+		
+		societaService.inserisciNuovo(nuovaSocieta);
+		
+		if (nuovaSocieta.getId() != 1) {
+			throw new RuntimeException("testInserisciNuovaSocieta failed!");
+		}
+		
+		societaService.rimuovi(nuovaSocieta);
+		System.out.println("testInserisciNuovaSocieta PASSED!");
+	}
 }
