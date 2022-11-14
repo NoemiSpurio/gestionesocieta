fb7205d614e7bb94139775731199320e30a0589e
NoemiSpurio <noemi.spurio@gmail.com>
2022-11-14 14:12:41 +0100

    creazione properties

--- src/main/java/it/prova/gestionesocieta/GestionesocietaApplication.java
+++ src/main/java/it/prova/gestionesocieta/GestionesocietaApplication.java
@@ -1,13 +1,20 @@
 package it.prova.gestionesocieta;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GestionesocietaApplication {
+public class GestionesocietaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionesocietaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
